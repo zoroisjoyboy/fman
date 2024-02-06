965df560f4c03683d3a49da0a392721f496d3dac
allensaakov <allengasa4@gmail.com>
2024-02-06 08:34:50 -0600

    Initial commit

---
+++ target/debug/deps/fman-eb10656ceb99f0b0.d
@@ -0,0 +1,5 @@
+C:\Users\allen\OneDrive\Desktop\projects\Rust\cli\fman\target\debug\deps\libfman-eb10656ceb99f0b0.rmeta: src\main.rs
+
+C:\Users\allen\OneDrive\Desktop\projects\Rust\cli\fman\target\debug\deps\fman-eb10656ceb99f0b0.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/fman-f7028837c7352270.d
@@ -0,0 +1,5 @@
+C:\Users\allen\OneDrive\Desktop\projects\Rust\cli\fman\target\debug\deps\libfman-f7028837c7352270.rmeta: src\main.rs
+
+C:\Users\allen\OneDrive\Desktop\projects\Rust\cli\fman\target\debug\deps\fman-f7028837c7352270.d: src\main.rs
+
+src\main.rs:
